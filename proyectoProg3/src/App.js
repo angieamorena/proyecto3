5a09adb53ddb0c90735231ed5cabe19d749c5a4b
TadeoBenzadon <tbenzadon@udesa.edu.ar>
2022-08-29 17:52:49 -0300

    hicde un par de cosas pero no puedo visualizar

--- proyectoProg3/src/App.js
+++ proyectoProg3/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import {Route, Switch} from 'react-router-dom'
 
 import './App.css';
 
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Home from './pages/Home/Home';
-import NotFound from './pages/NotFound/NotFound';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/pages/Home/Home';
+import NotFound from './components/pages/NotFound/NotFound';
 
 
 
 function App() {
   return (
     <div className="App">
+      <p>hola</p>
       <Header titulo="La Matanza" subtitulo="Multimedia"/>        
         {/* <Container /> */}
         <Switch>
--- proyectoProg3/src/components/Footer/Footer.js
+++ proyectoProg3/src/components/Footer/Footer.js
@@ -1,16 +1,10 @@
 import './Footer.css'
+import React from 'react'
 
 export default function Footer(){
 
-    //const estilo = 
-    const portada = {
-        textAlign: 'center'
-    }
-
-    
-
     return (
-        <div className="footer" style={portada} role="alert">
+        <div className="footer">
           <p>"Creado por Maria de Los Angeles Amorena, Jimena Freiman y Tadeo Benzadón"</p>
         </div>
     )
