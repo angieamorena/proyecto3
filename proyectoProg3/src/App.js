77639cbad21a29a03b0ec5c98533266965b1c955
TadeoBenzadon <tbenzadon@udesa.edu.ar>
2022-08-29 16:19:41 -0300

    limpie algunos archivos y cree components con las respectivas carpetas de footer, header y nav y tamb hice el not found, falta desarollar

--- proyectoProg3/src/App.js
+++ proyectoProg3/src/App.js
@@ -1,9 +1,31 @@
+import React from 'react'
+
 import './App.css';
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+
+
 
 function App() {
   return (
     <div className="App">
-      <p>prueba de que anda</p>
+      <Header titulo="La Matanza" subtitulo="Multimedia"/>        
+        {/* <Container /> */}
+        <Switch>
+          <Route path="/" exact  >
+            <Home />
+          </Route>
+          
+          {/* <Route path="/about" component={Characters} />  
+          <Route path="/characters/id/:id" component={CharacterDetails} />
+          <Route path="/contact" component={Contact}/> */}
+
+          <Route component={NotFound}/>
+        </Switch>
+      <Footer /> 
     </div>
   );
 }
---
+++ proyectoProg3/src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+export default function Footer(){
+
+    //const estilo = 
+    const portada = {
+        textAlign: 'center'
+    }
+
+    
+
+    return (
+        <div className="footer" style={portada} role="alert">
+          <p>"Creado por Maria de Los Angeles Amorena, Jimena Freiman y Tadeo Benzadón"</p>
+        </div>
+    )
+}
