a538b7bd641673441c97e0878198c3e05194b458
j29f123 <jfreiman@udesa.edu.ar>
2022-09-15 10:09:37 -0300

    css de detalles

--- proyectoProg3/src/components/Pelicula/Pelicula.js
+++ proyectoProg3/src/components/Pelicula/Pelicula.js
@@ -25,7 +25,6 @@ export default class Pelicula extends Component {
               <img src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={title}/> 
               <h2>{title}</h2>
               <p className={this.state.ver ?  `decripcionOculta` : `descripcion`}>{overview}</p>
-  
               <button className="btn btn-primary" onClick={()=>this.props.favorito(this.props.pelicula)}>Favoritos</button>
               <button className="btn btn-warning" onClick={()=> this.setState({ver:!this.state.ver})}>{this.state.ver ?  `Ver Mas` : `Ver Menos`}</button>
               <Link to={`/detallePelicula/id/${id}`} className="btn btn-warning" >Detalle</Link>
